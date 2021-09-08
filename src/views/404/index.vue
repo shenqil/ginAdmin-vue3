f2e7338919b990396e71fff8ea8f5e35ad566dd7
shenqi.lv <248120694@qq.com>
2021-09-08 23:25:23 +0800

    调整404页面

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <h5>404</h5>
+  <div style="margin-top: 200px">
+    <a-empty>
+      <template #description>
+        <h2>404</h2>
+      </template>
+      <a-button type="primary" @click="handleGoLogin">返回登录页</a-button>
+    </a-empty>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    handleGoLogin() {
+      this.$router.push({
+        name: "Login",
+        replace: true,
+      });
+    },
+  },
+};
+</script>
